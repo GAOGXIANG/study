cb596ccecccc198dc55f9717bd19eb0e03fb040d
gaogxiang <gaogxiang@163.com>
2019-08-09 14:26:29 +0800

    平方根及二分查找

--- datastructure/src/main/java/com/ggx/datastructure/search/BinarySearch.java
+++ datastructure/src/main/java/com/ggx/datastructure/search/BinarySearch.java
@@ -1,5 +1,10 @@
 package com.ggx.datastructure.search;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 二分查找
  * @author ggx
@@ -9,8 +14,8 @@ package com.ggx.datastructure.search;
 public class BinarySearch {
 
     /**
-     *
-     * @param key
+     * 实现模板1
+     * @param key 需要查找的值
      * @param a 数组必须是有序的
      * @return
      */
@@ -18,11 +23,21 @@ public class BinarySearch {
         int low = 0;
         int high = a.length - 1;
         while(low <= high){
-            int mid = low + (high - low) >>> 1;
+            int mid = low + ((high - low) >>> 1);
             if(key < a[mid]) high = mid -1;
             if(key > a[mid]) low = mid+1;
             if(key ==  a[mid]) return mid;
         }
         return -1;
     }
+    public static void main(String[] args) throws NoSuchMethodException {
+        List<String> foo = new ArrayList<>();
+        // 在类的外部这样获取
+        Type[] types = foo.getClass().getTypeParameters();
+        for(Type type : types) {
+            System.out.println(type);
+        }
+        System.out.println(foo.getClass().getTypeName());
+    }
+
 }
