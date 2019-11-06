8cf1d7b4909dc45b09f9f6bd6c712db6ce5ae483
gaogx <gaogxiang@163.com>
2019-11-06 22:37:25 +0800

    修复版

--- leetcode/src/main/java/com/ggx/leetcode/easy/dp/BestOrderAmount.java
+++ leetcode/src/main/java/com/ggx/leetcode/easy/dp/BestOrderAmount.java
@@ -4,28 +4,24 @@ import java.util.*;
 
 public class BestOrderAmount {
 
-    public static int[] bestOrderArray(int[] orders, int orderAmount){
+    public static List bestOrderArray(int[] orders, int orderAmount){
 
         Map<Integer, List<Integer>> orderAmountMap = new HashMap<>();
         Arrays.sort(orders);
-        for(int amount : orders){
-            List<Integer> list = new ArrayList<>();
-            list.add(amount);
-            orderAmountMap.put(amount, list);
-        }
-        for(int i = 1; i < orders.length; i++){
+        orderAmountMap.put(0, new ArrayList<>());
+        for(int i = 0; i < orders.length; i++){
             for(int j = orderAmount; j >= orders[i]; j--){
                 if(orderAmountMap.containsKey(j)){
                     continue;
                 }
                 if(orderAmountMap.containsKey(j - orders[i])){
-                    List<Integer> list = orderAmountMap.get(j - orders[i]);
+                    List<Integer> list = new ArrayList<>(orderAmountMap.get(j - orders[i]));
                     list.add(orders[i]);
                     orderAmountMap.put(j, list);
                 }
             }
         }
-        return new int[]{};
+        return orderAmountMap.get(orderAmount);
     }
 
     public static void main(String[] args){
